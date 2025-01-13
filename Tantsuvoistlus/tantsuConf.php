60cabb5d0e18a32f2f45d53ff9c2891e97a192ca
allikvaleria <154507539+allikvaleria@users.noreply.github.com>
2025-01-13 15:46:05 +0200

    Delete Tantsuvoistlus directory

--- Tantsuvoistlus/tantsuConf.php
+++
@@ -1,8 +0,0 @@
-<?php
-$kasutaja="valeriaallik";
-$parool="12345";
-$andmebaas="valeriaallik";
-$serverinimi="localhost";
-
-$yhendus=new mysqli($serverinimi,$kasutaja,$parool,$andmebaas);
-$yhendus->set_charset("utf8");
--- Tantsuvoistlus/tantsuConf2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$kasutaja="d132026_valeria";
-$parool="justsmiling2020";
-$andmebaas="d132026_valeria";
-$serverinimi="d132026.mysql.zonevs.eu";
-
-$yhendus=new mysqli($serverinimi,$kasutaja,$parool,$andmebaas);
-$yhendus->set_charset("utf8");
